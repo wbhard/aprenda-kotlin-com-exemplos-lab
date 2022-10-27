7e1fe37a2dba786c9b0e97693db60d7473623d69
wbhard <99848672+wbhard@users.noreply.github.com>
2022-10-26 22:05:37 -0300

    Adicionando parametros para os usuarios e possiveis ideias para matricula

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,35 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+//enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+data class Usuario(val firstName: String, val lastName: String, val id: Int){
+		fun usuario():String ="Nome completo: $nomeCompleto \nID: $id"
+    	val nomeCompleto = firstName + " " + lastName
+    //criar uma lista para armazenar os usuarios
 }
+//criar uma fun para realizar a matricula dos usuarios nos cursos
+	
+        
+
+	
+//fun matricular(usuario: Usuario) {
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+   // }
+
+
+//data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+
+//data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+
+   // val inscritos = mutableListOf<Usuario>()
+    
+
+//}
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //testes
+    val pessoa = Usuario("Luan","Moraes", 1)
+    println(pessoa.usuario())
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
