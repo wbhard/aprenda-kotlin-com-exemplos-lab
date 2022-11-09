81b3f4002d2992cc6db25e20313ff0d5ee7c873e
Luan Simenton <99848672+wbhard@users.noreply.github.com>
2022-11-09 20:52:53 -0300

    Alterações finais, entrega do desafio focado

--- desafio.kt
+++ desafio.kt
@@ -1,101 +1,59 @@
-//classe base
-//realização de testes ainda
-open class Usuario(firstName:String,lastName: String,id: Int,curso: String,nivel:String) {
-    val name: String = firstName + " " + lastName
-    val curso: String = curso
-    val nivel: String = nivel
-    val nomeComp = arrayListOf<String>(name) //foco agora é saber se é possivel realizar mais de uma fun dentro de uma class
-    fun addUser(name:String, curso:String) {
-    nomeComp.add(name+" "+curso)
-    return println("$name foi adicionado com sucesso ao curso $curso")}
-    val users: List<String> = nomeComp
-    fun getAddUser(): List<String> {                                  // 4
-    return users + curso
-}
-}
+enum class Level { BASIC, INTERMEDIARY, ADVANCED }
+enum class Post { STUDENT, TEACHER }
 
-//classe derivada
-class webDeveloper(firstName: String,lastName: String,id: Int,curso: String,nivel:String): Usuario(firstName,lastName,id,curso,nivel) {
-    fun website() {
-        if(curso=="webDeveloper" && nivel=="Basico"){
-        println("$name foi adicionado ao curso de $curso")
-    }        
-        else if(curso=="webDeveloper" && nivel=="Intermediario"){
-        println("$name foi adicionado ao curso de $curso")
+data class Users(val fNome: String, val lNome: String, val email: String, val post: Post)
+
+data class EducationalContent(val name: String, val duration: Int, val level: Level)
+
+data class Formation(val nome: String, val contents: List<EducationalContent>, val level: Level) {
+
+    val registered = mutableListOf<Users>()
+   
+    fun addUser(user: Users) {
+         registered.add(user)
     }
-        else if(curso=="webDeveloper" && nivel=="Avancado"){
-            println("$name foi adicionado ao curso de $curso")
-    }
-        else{
-            println("favor informar um curso valido")}
+   
+    fun getAddUser(){
+        for(user in registered){
+            println("|Nome: ${user.fNome+" "+user.lNome}| - |Email: ${user.email}| - (${user.post})")
         }
-}
-//classe derivada
-class androidDeveloper(firstName:String,lastName:String,id:Int,curso: String,nivel:String): Usuario(firstName,lastName,id,curso,nivel) {
-    fun android() {
-        if(curso=="androidDeveloper" && nivel=="Basico"){
-        println("$name foi adicionado ao curso de $curso")
-    }        
-        else if(curso=="androidDeveloper" && nivel=="Intermediario"){
-        println("$name foi adicionado ao curso de $curso")
     }
-        else if(curso=="androidDeveloper" && nivel=="Avancado"){
-            println("$name foi adicionado ao curso de $curso")
-    }
-        else{
-            println("favor informar um curso valido")}
+   
+    fun printEducationalContent(){
+        for(cont in contents){
+            println("${cont.name} - ${cont.duration}min - (${cont.level})")
         }
+    }
 }
-//classe derivada
-class iosDeveloper(firstName:String,lastName:String,id:Int,curso:String,nivel:String): Usuario(firstName,lastName,id,curso,nivel) {
-    fun iosapp() {
-        if(curso=="iosDeveloper" && nivel=="Basico"){
-        println("$name foi adicionado ao curso de $curso")
-    }        
-        else if(curso=="iosDeveloper" && nivel=="Intermediario"){
-        println("$name foi adicionado ao curso de $curso")
-    }
-        else if(curso=="iosDeveloper" && nivel=="Avancado"){
-            println("$name foi adicionado ao curso de $curso")
-    }
-        else{
-            println("favor informar um curso valido")}
-        }
-}
-//////////////////////////////////////////////////
-//Listagem
-val systemUsers: MutableList<String> = mutableListOf()        // 1
-                              // 2
-//Pulo do gato
 
-
-
-
-    /*addSystemUser(4)                                              // 5 
-    println("Tot sudoers: ${getSysSudoers().size}")               // 6
-    getSysSudoers().forEach {                                     // 7
-        i -> println("Some useful info on user $i")
-    }
-    // getSysSudoers().add(5) <- Error!  */                         // 8
-////////////////////////////////////////////////////
 fun main() {
-    //testes
-    var user = Usuario("Luan","Moraes",0,"teste","Teste")
-        user.addUser("Camili","Java")
-        user.addUser("Luana","Flutter")
-		val wd = webDeveloper("Luana","Simenton",1,"webDeveloper","Basico")
-        wd.website()
-   /* val wd = webDeveloper("Gennady", "Moraes",1,"webDeveloper","Intermediario")
-    wd.website()
-    val ad = androidDeveloper("Gaurav", "Simenton",2,"androidDeveloper","Avancado")
-    ad.android()
-    val iosd = iosDeveloper("Praveen", "Aparecida",3,"iosDeveloper","Basico")
-    iosd.iosapp()
-    
-    addSystemUser("Luana")
-    addSystemUser("Lucas")
-    print(User)*/
-    //user.getAddUser().forEach {                                     // 7
-    //    i -> println("$i - ")
-   // }
+ 	val moduleKotlinB = EducationalContent("Linguagem Kotlin", 120, Level.BASIC)
+    val moduleAndroidStudioB = EducationalContent("Fundamentos Android Studio", 120, Level.BASIC)
+  	val moduleKotlinI = EducationalContent("Linguagem Kotlin", 180, Level.INTERMEDIARY)
+  	val moduleAndroidStudioI = EducationalContent("Fundamentos Multiscreen", 180, Level.INTERMEDIARY)
+   
+	val contentList = mutableListOf<EducationalContent>()
+  		contentList.add(moduleKotlinB)
+  		contentList.add(moduleAndroidStudioB)
+    	contentList.add(moduleKotlinI)
+  		contentList.add(moduleAndroidStudioI)
+   
+    val luan = Users("Luan","Moraes", "luanmoraes@gmail.com", Post.STUDENT)
+    val camili = Users("Camili","Compagnoni", "camilicompagnoni@gmail.com", Post.STUDENT)
+   
+    val androidTraining = Formation("Formação Android Developer", contentList, Level.INTERMEDIARY)
+     
+    androidTraining.addUser(luan)
+    androidTraining.addUser(camili)
+   
+  	println(" -----------------------------------------")
+    println("|${androidTraining.nome} - ${androidTraining.level}|")
+    println(" -----------------------------------------")
+   
+    println("Grade do curso")
+    println(androidTraining.printEducationalContent())
+    println("******************************************")
+   
+    println("Usuarios cadastrados no curso")
+    println(androidTraining.getAddUser())
 }
